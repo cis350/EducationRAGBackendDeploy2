bf490565b347f1c0ac76ec46c968fba84d20a860
Neil Kapoor <40589159+nkapoor11@users.noreply.github.com>
2024-04-13 17:34:26 -0400

    created new verifyToken but didnt call it in server.js

--- Controller/Utils/auth.js
+++ Controller/Utils/auth.js
@@ -12,7 +12,7 @@ require('dotenv').config();
  * @param {Function} next - Callback function to continue to the next middleware.
  * @returns {Object} - Returns a JSON response object if the token is missing or invalid.
  */
-function verifyToken(req, res, next) {
+/*function verifyToken(req, res, next) {
   const authHeader = req.headers.authorization;
   const token = authHeader && authHeader.split(' ')[1]; // Bearer TOKEN_VALUE
 
@@ -28,6 +28,37 @@ function verifyToken(req, res, next) {
     return res.status(403).json({ message: 'Invalid Token.' });
   }
   return null;
+}*/
+
+function verifyToken(req, res, next) {
+  const authHeader = req.headers.authorization;
+  const token = authHeader && authHeader.split(' ')[1]; // Bearer TOKEN_VALUE
+
+  if (!token) {
+    //return res.status(401).json({ message: 'A token is required for authentication' });
+    return 3; // invalid token
+  }
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    console.log('payload', decoded);
+    req.email = decoded.email;
+    if (!req.email) {
+      // user is undefined
+      return 2;
+    }
+    next();
+    return 0; // token verified -- success
+
+  } catch (err) {
+    if (err.name === 'TokenExpiredError') {
+      console.log('error', err.message);
+      return 1;
+    }
+    // invalid token
+    console.log('error', err.message);
+    return 3;
+  }
 }
 
 /**
