ba1395d76f9993ef2f068745cea72d4b071591d1
Neil Kapoor <40589159+nkapoor11@users.noreply.github.com>
2024-05-06 20:11:07 -0400

    added cypress tests for chat history

--- Controller/client/cypress/e2e/students.cy.js
+++ Controller/client/cypress/e2e/students.cy.js
@@ -1,5 +1,6 @@
 describe('our first E2E test', () => {
   it('test signup flow', () => {
+    //cy.visit('http://localhost:3001/signup')
     cy.visit('https://education-rag-6f6c688e8b93.herokuapp.com/signup')
     //cy.get('#email').type('abcde@gmail.com')
     //cy.get('#username').type('abc')
@@ -39,6 +40,39 @@ describe('our first E2E test', () => {
     cy.contains('button', 'Logout').click()
     //cy.get('button').contains('Logout')
     //cy.contains('button', 'Logout').click()
+  })
+
+  it('Testing chat', () => {
+    //cy.visit('http://localhost:3001/login')
+    cy.visit('https://education-rag-6f6c688e8b93.herokuapp.com/login')
+    //cy.get('#email').type('abcde@gmail.com')
+    //cy.get('#password').type('abcdefghijk')
+    
+    cy.get('#email').type('neilkapoor11@gmail.com')
+    cy.get('#password').type('password123')
+    cy.contains('button', 'Log In').click();
+    
+    cy.contains('button', 'Settings').click()
+    cy.contains('button', 'Go to FAQ').click()
+    cy.contains('button', 'Back to Chat').click()
+    cy.contains('button', 'New Chat').click()
+    cy.get('input[type="text"]').type('chat2')
+    cy.contains('button', 'Confirm').click()
+
+    cy.contains('button', 'chat2').click()
+
+    cy.get('input[type="text"]').type('Can you help me with CIS 121?')
+    cy.contains('button', 'Send').click()
+
+    cy.get('input[type="text"]').type('What is a trie?')
+    cy.contains('button', 'Send').click()
+
+    cy.contains('button', 'Go to FAQ').click()
+    cy.contains('button', 'Back to Chat').click()
+    cy.contains('button', 'chat2').click()
+
+    cy.contains('button', 'X').click()
+    cy.contains('button', 'Logout').click()
   })
 
   
